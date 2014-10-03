399465c3cb484da103146758edaacd7283032b3f
Alex Gherghisan <alex.gherghisan@gmail.com>
2014-10-03 12:10:29 +0300

    Support void tags (e.g. <input>, <img>)

--- html-ellipsis.js
+++ html-ellipsis.js
@@ -20,6 +20,46 @@
 }(this, function factory() {
 	'use strict';
 
+	/** Extracts <tag id="foo"> from a larger string. Assumes str[startIdx] === '<' */
+	function extractTag(str, startIdx) {
+		var endIdx = str.indexOf('>', startIdx);
+		return str.slice(startIdx, endIdx + 1);
+	}
+
+	/** Checks that <tag> is an end tag */
+	function isEndTag(tag) {
+		return tag[1] === '/';
+	}
+
+	/** Extracts tag from <tag id="foo"> */
+	function extractTagName(tag) {
+		var tagNameEndIdx = tag.indexOf(' ');
+		if (tagNameEndIdx === -1) {
+			// check for <br/> style tags
+			tagNameEndIdx = tag.indexOf('/');
+
+			if (tagNameEndIdx === -1) {
+				tagNameEndIdx = tag.length - 1;
+			}
+		}
+
+		return tag.slice(1, tagNameEndIdx);
+	}
+
+	// taken from http://webdesign.about.com/od/htmltags/qt/html-void-elements.htm
+	var voidTags = ['area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'wbr'];
+
+	/** Checks that tagName is a void tag (it doesn't have an end tag) */
+	function isVoidTag(tagName) {
+		for (var i = voidTags.length - 1; i >= 0; --i) {
+			if (tagName === voidTags[i]) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	/**
 	 * Truncates a string to maxLength chars without destroying HTML tags.
 	 * Optionally adds ellipsis to the end of the string (this does not make the string larger than maxLength)
@@ -45,18 +85,22 @@
 		var charCount = 0;
 		var tagStack = [];
 
-		while (i < len - 1 && charCount < maxLength) {
+		while (i < len && charCount < maxLength) {
 			if (html[i] === '<') {
-				var tagClose = html.indexOf('>', i);
+				var tag = extractTag(html, i);
 
-				if (html[i + 1] === '/') {
-					// assume valid HTML
+				// skip content between < and >
+				i += tag.length;
+
+				if (isEndTag(tag)) {
 					tagStack.pop();
 				} else {
-					tagStack.push(html.slice(i + 1, tagClose));
-				}
+					var tagName = extractTagName(tag);
 
-				i = tagClose + 1;
+					if (!isVoidTag(tagName)) {
+						tagStack.push(tagName);
+					}
+				}
 			} else {
 				++charCount;
 				++i;
@@ -66,17 +110,7 @@
 		var result = html.slice(0, i);
 
 		for (var j = tagStack.length - 1; j >= 0; --j) {
-			var tag = tagStack[j];
-
-			// extract tagName from <tagName id="maybe">
-			var tagName;
-			var spaceIdx = tag.indexOf(' ');
-			if (spaceIdx > -1) {
-				tagName = tag.slice(0, spaceIdx);
-			} else {
-				tagName = tag;
-			}
-			result += '</' + tagName + '>';
+			result += '</' + tagStack[j] + '>';
 		}
 
 		if (opt_addEllipsis && result.length < html.length) {
